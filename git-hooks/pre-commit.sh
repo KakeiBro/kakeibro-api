ef8bea7775e9a75bc1797da045a240a9e70819b8
Diego Balderrama <diegowrhasta@gmail.com>
2025-02-14 13:22:26 -0400

    Adds bash (unix-based) scripts for hooks

--- git-hooks/pre-commit.sh
+++ git-hooks/pre-commit.sh
@@ -1,10 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-#echo "Running .NET format check..."
-#dotnet format --verify-no-changes || exit 1
+# Change to the Kakeibro.API directory
+cd ./Kakeibro.API || { echo "Failed to change directory"; exit 1; }
 
-#echo "Running .NET build check..."
-#dotnet build /warnaserror || exit 1
+echo "Running .NET format check..."
+dotnet format --verify-no-changes
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
-#echo "Pre-commit checks passed!"
-echo "WE ARE AT LINUX!"
+echo "Running .NET build check..."
+dotnet build
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo "Running .NET test check..."
+dotnet test
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo "Pre-commit checks passed!"
--- git-hooks/pre-push.sh
+++ git-hooks/pre-push.sh
@@ -1,10 +1,43 @@
-#!/bin/sh
+#!/bin/bash
 
-#echo "Running .NET format check..."
-#dotnet format --verify-no-changes || exit 1
+# Change to the Kakeibro.API directory
+cd ./Kakeibro.API || { echo "Failed to change directory"; exit 1; }
 
-#echo "Running .NET build check..."
-#dotnet build /warnaserror || exit 1
+echo "Running .NET format check..."
+dotnet format --verify-no-changes
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
-#echo "Pre-commit checks passed!"
-echo "WE ARE AT LINUX!"
+echo "Running .NET build check..."
+dotnet build
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo "Running .NET test check..."
+dotnet test
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo "Running .NET outdated package check... (warning)"
+dotnet list package --outdated
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo "Running .NET migrations..."
+# Uncomment the following line to enable migrations check
+# dotnet ef migrations list
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo "Running .NET vulnerability check..."
+dotnet list package --vulnerable
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo "Pre-push checks passed!"
