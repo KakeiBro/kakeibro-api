cb3e7c9162d3bce374db8bc44133ff67ae9496fc
Diego Balderrama <diegowrhasta@gmail.com>
2025-03-04 10:49:19 -0400

    Fixes possible edge case with failing post-commit script

--- git-hooks/post-commit.ps1
+++ git-hooks/post-commit.ps1
@@ -8,10 +8,9 @@ $jsonBakFilePath = ".\src\KakeiBro.API\appsettings.bak.json"
 # Replace the file with the backup
 if (Test-Path $jsonBakFilePath) {
     Copy-Item -Path $jsonBakFilePath -Destination $jsonFilePath -Force
+    Remove-Item -Path $jsonBakFilePath -Force
 } else {
     Write-Host "Backup file not found: $jsonBakFilePath"
 }
-
-Remove-Item -Path $jsonBakFilePath -Force
 
 Write-Host "Post-commit operations done!"
