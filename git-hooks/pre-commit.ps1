2aef4ab9c02dcbff43ba41d7f5f2ff3879204134
Diego Balderrama <diegowrhasta@gmail.com>
2025-03-04 11:14:47 -0400

    Adds further windows fixes for jq

--- git-hooks/pre-commit.ps1
+++ git-hooks/pre-commit.ps1
@@ -13,6 +13,7 @@ dotnet test
 if ($LASTEXITCODE -ne 0) { exit 1 }
 
 Write-Host "Running Google OAuth credential sanitization..."
+
 # Ensure jq is installed
 if (-not (Get-Command jq -ErrorAction SilentlyContinue)) {
     Write-Host "Error: jq is not installed. Install it using 'choco install jq' (Windows)."
@@ -26,9 +27,10 @@ $jsonBakFilePath = ".\src\KakeiBro.API\appsettings.bak.json"
 Copy-Item -Path $jsonFilePath -Destination $jsonBakFilePath -Force
 
 # Use jq to modify the JSON and format it
-$jqFilter = '(.GoogleAuth.ClientId,.GoogleAuth.RedirectUri,.GoogleAuth.JavascriptOrigin) |= ""'
-jq $jqFilter "$jsonFilePath" | Out-File -Encoding utf8 "$jsonFilePath"
+$jqFilter = '(.GoogleAuth.ClientId, .GoogleAuth.RedirectUri, .GoogleAuth.JavascriptOrigin) |= ""'
+jq $jqFilter $jsonFilePath | Out-File -Encoding utf8 "$jsonFilePath"
 
+# Stage the modified file in Git
 git add $jsonFilePath
 
 Write-Host "Pre-commit checks passed!"
