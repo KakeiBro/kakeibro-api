67fde4107104d5ee5feb5983cd45d2cf28d75c89
Diego Balderrama <diegowrhasta@gmail.com>
2025-03-04 11:22:46 -0400

    Fixes powershell issues with double quotes

--- git-hooks/pre-commit.ps1
+++ git-hooks/pre-commit.ps1
@@ -21,14 +21,14 @@ if (-not (Get-Command jq -ErrorAction SilentlyContinue)) {
 }
 
 $jsonFilePath = ".\src\KakeiBro.API\appsettings.json"
-$jsonBakFilePath = ".\src\KakeiBro.API\appsettings.bak.json"
+$jsonBakFilePath = '.\src\KakeiBro.API\appsettings.bak.json'
 
 # Create a backup of the original JSON
 Copy-Item -Path $jsonFilePath -Destination $jsonBakFilePath -Force
 
 # Use jq to modify the JSON and format it
-$jqFilter = '(.GoogleAuth.ClientId, .GoogleAuth.RedirectUri, .GoogleAuth.JavascriptOrigin) |= ""'
-jq $jqFilter $jsonFilePath | Out-File -Encoding utf8 "$jsonFilePath"
+$jqFilter = '(.GoogleAuth.ClientId, .GoogleAuth.RedirectUri, .GoogleAuth.JavascriptOrigin) |= """"' # PW doesn't like ", you have to escape it by adding double ""
+jq $jqFilter $jsonFilePath | Out-File -Encoding utf8 $jsonFilePath
 
 # Stage the modified file in Git
 git add $jsonFilePath
