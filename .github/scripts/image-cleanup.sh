5ab6dd0ac82844f3a91ca5d7928cb22cb242b726
Diego Balderrama <diegowrhasta@gmail.com>
2025-03-04 21:21:33 -0400

    Adds better clean up for image registry (use images instead of tags)

--- .github/scripts/image-cleanup.sh
+++ .github/scripts/image-cleanup.sh
@@ -12,31 +12,29 @@ echo "Authenticating to Google Cloud..."
 gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
 
 # Get the list of tags for the specified package, sorted by creation time in descending order
-echo "Fetching tags for package..."
-TAGS=$(gcloud artifacts tags list \
+echo "Fetching images from repository..."
+IMAGES=$(gcloud artifacts docker images list $REPOSITORY_NAME \
   --project=$PROJECT_ID \
   --location=$REGION \
-  --repository=$REPOSITORY_NAME \
-  --package=$IMAGE_NAME \
-  --format="value(name)" | tac)
+  --format="value(DIGEST)" \
+  --sort-by="~CREATE_TIME")
 
-# Convert the list of tags into an array
-TAGS_ARRAY=($TAGS)
+# Convert the list of digests into an array
+IMAGE_ARRAY=($IMAGES)
 
-# Check if there are more than 3 tags
-if [ ${#TAGS_ARRAY[@]} -gt 3 ]; then
-  echo "Found ${#TAGS_ARRAY[@]} tags. Keeping the 3 most recent ones and deleting the rest..."
+# Check if there are more than 3 images
+if [ ${#IMAGE_ARRAY[@]} -gt 3 ]; then
+  echo "Found ${#IMAGE_ARRAY[@]} images. Keeping the 3 most recent ones and deleting the rest..."
 
   # Loop through the tags, skipping the first 3 (most recent)
-  for ((i=3; i<${#TAGS_ARRAY[@]}; i++)); do
-    TAG=${TAGS_ARRAY[$i]}
-    echo "Deleting tag: $TAG"
-    gcloud artifacts tags delete $TAG \
+  for ((i=3; i<${#IMAGE_ARRAY[@]}; i++)); do
+    DIGEST=${IMAGE_ARRAY[$i]}
+    echo "Deleting image: $DIGEST"
+    gcloud artifacts docker images delete "$REPOSITORY_NAME@$DIGEST" \
       --project=$PROJECT_ID \
       --location=$REGION \
-      --repository=$REPOSITORY_NAME \
-      --package=$IMAGE_NAME \
-      --quiet
+      --quiet \
+      --force-delete-tags
   done
 else
   echo "Only ${#TAGS_ARRAY[@]} tags found. No cleanup needed."
